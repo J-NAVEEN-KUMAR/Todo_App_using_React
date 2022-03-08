b23c50d9d041d80276141e704c0c2b9ba702f4cc
J-NAVEEN-KUMAR <naveen.jeruban@gmail.com>
2022-03-08 21:10:23 +0530

    added timeagojs

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,42 +1,49 @@
-import React, { useState } from 'react';
-import TodoForm from './TodoForm';
-import { RiCloseCircleLine } from 'react-icons/ri';
-import { TiEdit } from 'react-icons/ti';
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
+import TimeAgo from "timeago-react";
 
 const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
   const [edit, setEdit] = useState({
     id: null,
-    value: ''
+    value: "",
   });
 
-  const submitUpdate = value => {
+  const submitUpdate = (value) => {
     updateTodo(edit.id, value);
     setEdit({
       id: null,
-      value: ''
+      value: "",
     });
   };
 
   if (edit.id) {
     return <TodoForm edit={edit} onSubmit={submitUpdate} />;
   }
+  const currentDate = new Date();
 
   return todos.map((todo, index) => (
     <div
-      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
       key={index}
     >
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+      <div
+        key={todo.id}
+        onClick={() => completeTodo(todo.id)}
+        className="taskContainer"
+      >
         {todo.text}
       </div>
-      <div className='icons'>
+      <TimeAgo date={currentDate} className="currentDate" />
+      <div className="icons">
         <RiCloseCircleLine
           onClick={() => removeTodo(todo.id)}
-          className='delete-icon'
+          className="delete-icon"
         />
         <TiEdit
           onClick={() => setEdit({ id: todo.id, value: todo.text })}
-          className='edit-icon'
+          className="edit-icon"
         />
       </div>
     </div>
